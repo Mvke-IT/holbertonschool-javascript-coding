6e727c147b15790691f49a649924e946b5e29220
Mvke-IT <michael.duarte09@gmail.com>
2024-05-15 11:49:46 -0400

    fixing indentation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
+const file = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
